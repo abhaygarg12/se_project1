8febebf1cc71457b0c0af2fdf0f4878ef962d8ff
madhavkh14 <madhavkhandelwa70@gmail.com>
2021-04-27 01:49:39 +0530

    Madhav

--- SE_Project/Hostel/models.py
+++ SE_Project/Hostel/models.py
@@ -5,5 +5,6 @@ from django.db import models
 class Complaints(models.Model):
     rollno = models.ForeignKey(Students, on_delete = models.CASCADE)
     description = models.CharField(max_length=100)
+    date_created = models.DateField(auto_now_add=True)
     location = models.CharField(max_length=10)'''
     
