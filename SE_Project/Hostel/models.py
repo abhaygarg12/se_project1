e0288d87b70ff901f59ec299c8dfb2b34a8fedcd
madhavkh14 <madhavkhandelwa70@gmail.com>
2021-04-27 13:34:55 +0530

    Madhav

--- SE_Project/Hostel/models.py
+++ SE_Project/Hostel/models.py
@@ -5,10 +5,10 @@ from django.core.validators import MinLengthValidator,MaxLengthValidator
 class Students(models.Model):
     roll_no = models.IntegerField(validators=[MinLengthValidator(9),MaxLengthValidator(9)], primary_key=True)
     student_name = models.CharField(max_length=30)
-    fathers_name = models.CharField(max_length=30)
     dob = models.DateField(max_length=10,help_text="format : DD-MM-YYYY")
     mobile = models.IntegerField(validators=[MinLengthValidator(10),MaxLengthValidator(10)])
-    username = models.EmailField(max_length=50)
+    username = models.CharField(max_length=20)
+    email = models.EmailField(max_length=50)
     room_no = models.CharField(max_length=5)
 
     def __str__(self):
