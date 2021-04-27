a051509679a912ce96cbb7c8de815ac1cc8905b1
madhavkh14 <madhavkhandelwa70@gmail.com>
2021-04-27 23:30:14 +0530

    Madhav

--- SE_Project/Hostel/models.py
+++ SE_Project/Hostel/models.py
@@ -22,7 +22,8 @@ class Complaints(models.Model):
     }
 
     rollno = models.ForeignKey(Students, on_delete = models.CASCADE)
-    description = models.CharField(max_length=100)
+    title = models.CharField(max_length=30, default= "")
+    description = models.CharField(max_length=150)
     date_created = models.DateField(auto_now_add=True)
     location = models.CharField(max_length=20)
     status = models.CharField(max_length=20, choices=STATUS)
