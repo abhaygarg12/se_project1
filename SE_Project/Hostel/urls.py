5f99f0e92eb2c8f9dc2b1eaa3f5a674a7808e02b
rishavchaudhary007 <rishavchaudhary0712@gmail.com>
2021-04-30 13:40:51 +0530

    YE dekh password forget hogya

--- SE_Project/Hostel/urls.py
+++ SE_Project/Hostel/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
@@ -22,5 +23,16 @@ urlpatterns = [
 
     path('new_complaint/', views.new_complaint, name='new-complaint'),
 
+
+    path('reset_password/', auth_views.PasswordResetView.as_view(), name="reset_password"),
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(), name="password_reset_done"),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+ 
+
+
 ]
+
+
+
 
